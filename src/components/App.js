820a9048eb404500967bc1b888244acd205c60a9
maqsudinamdar <maqsudinamdar@gmail.com>
2021-08-30 01:25:11 +0530

    feat : GoogleAuth for Auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './Header';
 
 import StreamCreate from './streams/StreamCreate';
 import StreamDelete from './streams/StreamDelete';
@@ -11,9 +13,10 @@ import StreamShow from './streams/StreamShow';
 const App = () => {
 
     return (
-        <div>
+        <div className="ui container">
             <BrowserRouter>
                 <div>
+                    <Header />
                    <Route path="/" exact component={StreamList} /> 
                    <Route path="/streams/new" component={StreamCreate} /> 
                    <Route path="/streams/edit" component={StreamEdit} />
