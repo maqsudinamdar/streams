2378a6124243407bc43cde86998ea96e1f71c4d1
maqsudinamdar <maqsudinamdar@gmail.com>
2021-09-04 01:06:51 +0530

    feat : implement flv

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -7,12 +7,35 @@ import { getStream } from '../../actions';
 class StreamShow extends React.Component {
 
     constructor(props) {
-        super(pros);
+        super(props);
         this.videoRef = React.createRef();
     }
     componentDidMount () {
-        this.props.getStream(this.props.match.params.id);
+        const { id } = this.props.match.params;
+
+        this.props.getStream(id);
+        this.buildPlayer();
     }
+
+    componentDidUpdate() {
+        this.buildPlayer();
+    }
+
+
+    buildPlayer() {
+        if (this.player || !this.props.stream) {
+            return;
+        }
+
+        const { id } = this.props.match.params;
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        });
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
+    }
+
 
     render() {
         
